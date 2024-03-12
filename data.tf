6d06dbcdc52e9a9aa565e32822f166ac4f2b640f
Sylvester-agbo <71844379+Sylvester-agbo@users.noreply.github.com>
2024-03-12 04:47:39 -0600

    Create data.tf

---
+++ data.tf
@@ -0,0 +1,22 @@
+data "aws_caller_identity" "current" {}
+
+
+data "aws_iam_policy_document" "topic" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    actions   = ["SNS:Publish"]
+    resources = ["arn:aws:sns:*:*:s3-event-notification-topic"]
+
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = [aws_s3_bucket.backend[0].arn]
+    }
+  }
+}
