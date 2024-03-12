90fadf7c71743271446d33d91a8ea62d59627edf
Sylvester-agbo <71844379+Sylvester-agbo@users.noreply.github.com>
2024-03-12 04:44:03 -0600

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -8,12 +8,28 @@ variable "region" {
 
 variable "create_vpc" {
   type    = bool
-  default = true
 }
 
-variable "kms_master_key_id" {
+variable "env" {
   type = string
 }
-variable "bucket" {
+
+variable "region" {
   type = string
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "life_cycle" {
+  type = bool
+}
+
+variable "logging_bucket" {
+  type = string
+}
+
+variable "versioning" {
+  type = bool
 }
